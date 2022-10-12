026398c4b3b89a82bab92216f14cba8133859ceb
TheHappyLife <dangnguyen120699@gmail.com>
2022-10-12 11:02:14 +0700

    v5

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,8 @@ import { useEffect, useState } from "react";
 import BackgroundVideo from "../components/BackgroundVideo";
 import Content from "../components/Content";
 import IntroducePage from "../components/IntroducePage";
-import { NextSeo } from 'next-seo';
-import SEO from '../next-seo.config';
+import { NextSeo } from "next-seo";
+import SEO from "../next-seo.config";
 import SubmitPopup from "../components/SubmitPopup";
 import Seo from "../components/Seo";
 const landingPage = {
@@ -175,8 +175,45 @@ export default function Home() {
         position: "relative",
       }}
     >
-      {/* <NextSeo {...SEO} /> */}
-      <Seo/>
+      <NextSeo
+        title="Video Page Title"
+        description="Description of video page"
+        openGraph={{
+          title: "Open Graph Video Title",
+          description: "Description of open graph video",
+          url: "https://www.example.com/videos/video-title",
+          type: "video.movie",
+          video: {
+            // Multiple Open Graph actors is only available in version `7.0.2-canary.35`+ of next
+            actors: [
+              {
+                profile: "https://www.example.com/actors/@firstnameA-lastnameA",
+                role: "Protagonist",
+              },
+              {
+                profile: "https://www.example.com/actors/@firstnameB-lastnameB",
+                role: "Antagonist",
+              },
+            ],
+            // Multiple Open Graph directors is only available in version `7.0.2-canary.35`+ of next
+            directors: [
+              "https://www.example.com/directors/@firstnameA-lastnameA",
+              "https://www.example.com/directors/@firstnameB-lastnameB",
+            ],
+            // Multiple Open Graph writers is only available in version `7.0.2-canary.35`+ of next
+            writers: [
+              "https://www.example.com/writers/@firstnameA-lastnameA",
+              "https://www.example.com/writers/@firstnameB-lastnameB",
+            ],
+            duration: 680000,
+            releaseDate: "2022-12-21T22:04:11Z",
+            // Multiple Open Graph tags is only available in version `7.0.2-canary.35`+ of next
+            tags: ["Tag A", "Tag B", "Tag C"],
+          },
+          site_name: "SiteName",
+        }}
+      />
+      {/* <Seo/> */}
       <Box
         className="buffer-bg"
         sx={{
